7363b1605aebd4e0e369d50a627a261185befbd4
Gabriel <gabrieldavid_98@hotmail.com>
2021-01-24 22:38:04 -0500

    add lazy-seq

--- src/elo/core.clj
+++ src/elo/core.clj
@@ -14,8 +14,6 @@
 (defn match-probability [player-1-rating player-2-rating]
   (/ 1 (+ 1 (math/expt 10 (/ (- player-2-rating player-1-rating) 400)))))
 
-(def k-factor 32)
-
 (def csv "resources/match_scores_1991-2016_unindexed_csv.csv")
 
 (defn recalculate-rating [K previuos-rating expected-outcome real-outcome]
@@ -59,5 +57,44 @@
                   :predictable-match-count 0
                   :correct-predictions 0}))))
 
+(defn elo-db
+  ([csv k]
+   (with-open [r (io/reader csv)]
+     (->> (csv/read-csv r)
+          sc/mappify
+          (sc/cast-with {:winner_sets_won sc/->int
+                         :loser_sets_won sc/->int
+                         :winner_games_won sc/->int
+                         :loser_games_won sc/->int})
+          (reduce (fn [{:keys [players] :as acc} {:keys [:winner_name :winner_slug
+                                                         :loser_name :loser_slug] :as match}]
+                    (let [winner-rating (get players winner_slug 400)
+                          loser-rating (get players loser_slug 400)
+                          winner-probability (match-probability winner-rating loser-rating)
+                          loser-probability (- 1 winner-probability)]
+                      
+                      (-> acc
+                          (assoc-in [:players winner_slug] (recalculate-rating k winner-rating winner-probability 1))
+                          (assoc-in [:players loser_slug] (recalculate-rating k loser-rating loser-probability 0))
+                          (update :matches (fn [ms]
+                                             (conj ms (assoc match
+                                                             :winner_rating winner-rating
+                                                             :loser_rating loser-rating)))))))
+                  {:players {}
+                   :matches []})
+          :matches
+          reverse))))
 
+(defn player-in-match? [{:keys [winner_slug loser_slug]} player-slug]
+  ((hash-set winner_slug loser_slug) player-slug))
 
+(defn match-tree-by-player [m player-slug]
+  (lazy-seq
+   (cond
+     (empty? m) '()
+     (player-in-match? (first m) player-slug) 
+     (cons (first m)
+           (cons [(match-tree-by-player (rest m) (:winner (first m)))
+                  (match-tree-by-player (rest m) (:loser (first m)))]
+                 '()))
+     :else (match-tree-by-player (rest m) player-slug))))
